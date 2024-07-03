ffc10107e829e9d63f63b3f4917a5d6d871bca8d
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-07-03 08:02:01 -0300

    .

--- fazendeiroram.js
+++ fazendeiroram.js
@@ -1,6 +1,6 @@
 // script.js
 function saibaMais() {
-    window.location.href = 'https://www.youtube.com/watch?v=OJdDLqzTHNQ.html'; // Substitua pelo URL real da página "Saiba Mais"
+    window.location.href = 'saiba_mais.html'; // Substitua pelo URL real da página "Saiba Mais"
 }
 
 function contato() {
